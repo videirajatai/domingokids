14dc0c1374408d25e723744e789ef9257122e734
Igreja Videira Jataí <secretariavideirajataigo@gmail.com>
2026-07-07 09:12:15 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,8 @@ const SUPABASE_URL = 'https://mxuvvsklqaelvxmpnzhe.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14dXZ2c2tscWFlbHZ4bXBuemhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNzA4NTMsImV4cCI6MjA5ODY0Njg1M30.ugrG6Q9Q3k7j9kKC1uRkWKtTX0TCTMkz_t8O_JF-fos';
 //const IMGBB_API_KEY = 'd3ec802e2dfc1896a26e44b80d489f17';
 
+const USB_URL = SUPABASE_URL;
+const SB_KEY = SUPABASE_ANON_KEY;
+
 // Cria o cliente e guarda no objeto window para uso global
 //window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
