e920b6541be527f96050ec9bff2b3d9cc0168f27
Igreja Videira Jataí <secretariavideirajataigo@gmail.com>
2026-07-28 15:26:48 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // app.js - Inicialização do cliente Supabase
-const SUPABASE_URL = 'https://mxuvvsklqaelvxmpnzhe.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14dXZ2c2tscWFlbHZ4bXBuemhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwNzA4NTMsImV4cCI6MjA5ODY0Njg1M30.ugrG6Q9Q3k7j9kKC1uRkWKtTX0TCTMkz_t8O_JF-fos';
+const SUPABASE_URL = 'https://asriklhdbzxubdcuauoz.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzcmlrbGhkYnp4dWJkY3VhdW96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyNTkzNzcsImV4cCI6MjEwMDgzNTM3N30.vhOVWm--45F7CTthWG4pZJdqo4DDcG6vMnua1hYSUuM';
 //const IMGBB_API_KEY = 'd3ec802e2dfc1896a26e44b80d489f17';
 
 const SB_URL = SUPABASE_URL;
